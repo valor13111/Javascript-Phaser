dd5a3ad8d4a442dd52c6fdc80e113878dd1f8ce1
Tyler Reed <valor13111@gmail.com>
2017-07-18 18:30:50 -0400

    Renamed breakout_phaser.js to breakout.js and added a level function to retrieve JSON data for stage.

--- Breakout/assets/js/breakout.js
+++ Breakout/assets/js/breakout.js
@@ -1,290 +1,298 @@
 /**
- * Created by Tyler on 7/10/2017.
+ * Using Phaser to aid in JavaScript game development.
+ *
+ * Phaser automatically creates a canvas element when initializing a new Phaser.Game object
+ * and assigning it to the game variable.
  */
 
-// variables to get canvas id and set it to 2d
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+var game = new Phaser.Game(800, 600, Phaser.AUTO, null, {
+    preload: preload,
+    create: create,
+    update: update
+});
 
-// defines starting point of ball at bottom center
-var ballPositionX = canvas.width / 2;
-var ballPositionY = canvas.height - 30;
+// initialize variable to represent ball and paddle
+var ball;
+var paddle;
+var ballAngle;
 
-// values to be added to every frame
-var dx = 2;
-var dy = -2;
+// velocities of ball object, positive moves 'down', negative moves 'up'
+var velocityX = 250;
+var velocityY = -250;
 
-// ball radius for collision detection
-var ballRadius = 10;
+// brick variables
+var bricks;
+var newBrick;
+var brickInfo;
+var brickWidth;
+var brickHeight;
+var brickRows;
+var brickColumns;
+var brickOffsetTop;
+var brickOffsetLeft;
+var brickPadding;
 
-// ball color
-var ballColor = "#0e36bd";
-
-// paddle width, height, and starting point X axis
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth) / 2;
-
-// paddle color and speed it moves
-var paddleColor = "#25d8b9";
-var paddleSpeed = 7;
-
-// bricks
-var brickRowCount = 3;
-var brickColumnCount = 5;
-var brickWidth = 75;
-var brickHeight = 25;
-var brickPadding = 10;
-var brickOffsetTop = 32;
-var brickOffsetLeft = 32;
-var brickColor = "#f65c0c";
-var bricks = [];
-createBricks2dArray();
-
-// left and right arrow button variables
-var rightPressed = false;
-var leftPressed = false;
-
-// tracking the score
+// score variables
+var scoreText;
 var score = 0;
+var scoreIncrement = 10;
 
-// tracking player lives
-var lives = 1;
+// lives variables
+var livesText;
+var lifeLostText;
+var lives = 3;
+
+// text style variable
+var textStyle = {
+    font: '18px Arial',
+    fill: '#6b9add'
+}
+
+var playing = false;
+var startButton;
+
+// sound variables
+var music;
+
+// emitter variables
+var emitter;
+
+var levelData;
 
 /**
- * Produces a random color with RGB, and is set to have to a higher chance of
- * lighter colors.
+ * Takes care of preloading the assets.  NO_SCALE sets to default size, whereas
+ * SHOW_ALL will show the game area while maintaining the original aspect ratio of the screen.
+ */
+function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
+    game.scale.pageAlignVertically = true;
+    game.scale.pageAlignHorizontally = true;
+
+    // gives name to the images
+    game.load.image('ball', 'assets/images/ball.png');
+    game.load.image('paddle', 'assets/images/paddle.png');
+    game.load.image('brick', 'assets/images/brick.png');
+    game.load.spritesheet('ball', 'assets/images/spritesheets/wobble.png', 20, 20);
+    game.load.spritesheet('button', 'assets/images/spritesheets/button.png', 120, 40);
+
+    // gives names to audio
+    game.load.audio('music', 'assets/sound/AllOfUS.mp3');
+
+    // background
+    game.load.image('city', 'assets/images/backgrounds/ruined_city.png');
+
+    // load JSON file
+    game.load.text('level', 'assets/data/level.json');
+}
+
+/**
+ * Executed once when everything is loaded and ready.
+ */
+function create() {
+    // initializes arcade physics engine
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    // background sprite
+    city = game.add.sprite(game.world.width * 0.5, game.world.height * 0.5, 'city');
+    city.width = game.world.width;
+    city.anchor.set(0.5);
+
+    // parse the JSON file
+    levelData = JSON.parse(game.cache.getText('level'));
+
+    // create the sprite by adding it to the game
+    // game.world.width or height is the canvas width or height, but could be used for offscreen
+    // interaction if the world is made bigger
+    // anchor sets the position of the object from top-left of that object, so its set
+    // to be in the middle for the ball, and at the bottom middle for paddle
+    ball = game.add.sprite(game.world.width * levelData.ballStart.x, game.world.height + levelData.ballStart.y, 'ball');
+    ball.animations.add('wobble', [0,1,0,2,0,1,0,2,0], 24);
+    ball.anchor.set(0.5);
+    paddle = game.add.sprite(game.world.width * levelData.paddleStart.x, game.world.height + levelData.paddleStart.y, 'paddle');
+    paddle.anchor.set(0.5, 1);
+
+    scoreText = game.add.text(5, 5, 'Points: 0', textStyle);
+
+    livesText = game.add.text(game.world.width - 5, 5, 'Lives: ' + lives, textStyle);
+    livesText.anchor.set(1, 0);
+    lifeLostText = game.add.text(game.world.width * 0.5, game.world.height * 0.5, 'Life lost, click to continue', textStyle);
+    lifeLostText.anchor.set(0.5);
+    lifeLostText.visible = false;
+
+    startButton = game.add.button(game.world.width * 0.5, game.world.height * 0.5, 'button', startGame, this, 1, 0, 2);
+    startButton.anchor.set(0.5);
+
+    // background music
+    music = game.sound.add('music');
+    music.volume = 0.3;
+    music.loop = true;
+
+    // create an emitter and adjust settings
+    emitter = game.add.emitter(ball.x, ball.y, 1);
+    emitter.makeParticles('ball');
+    ball.addChild(emitter);
+    emitter.setAlpha(0.3, 0.5);
+    emitter.y = 0;
+    emitter.x = 0;
+    emitter.lifespan = 300;
+    emitter.maxParticleSpeed = new Phaser.Point(-100, 50);
+    emitter.minParticleSpeed = new Phaser.Point(-200, -50);
+
+    // enables the ball for physics system, which isn't enabled by default
+    // set the velocity of the ball
+    // allow for ball to collide with edges of the canvas, and set it to bounce off walls
+    game.physics.enable(ball, Phaser.Physics.ARCADE);
+    game.physics.enable(paddle, Phaser.Physics.ARCADE);
+    ball.body.collideWorldBounds = true;
+    ball.body.bounce.set(1);
+    paddle.body.immovable = true;
+
+    // disables collision with bottom wall,
+    // checks if ball hits bottom bounds, and executes
+    // the function given
+    game.physics.arcade.checkCollision.down = false;
+    ball.checkWorldBounds = true;
+    ball.events.onOutOfBounds.add(ballLeaveScreen, this);
+
+    initBricks();
+}
+
+/**
+ * Executed on every frame.
+ */
+function update() {
+    game.physics.arcade.collide(ball, paddle, ballHitPaddle);
+
+    // if score is greater than a certain number, emit a trail particle and increase speed
+    if (score >= 0) {
+        emitter.emitParticle();
+    }
+
+    ballAngle = game.physics.arcade.angleBetween(ball, paddle) * 100;
+    emitter.angle = ballAngle;
+
+    // when the ball collides with a brick, check the function, which removes the brick.
+    game.physics.arcade.collide(ball, bricks, ballHitBrick);
+    if (playing) {
+        paddle.x = game.input.x || game.world.width * 0.5;
+    }
+}
+
+/**
+ * Initializes the bricks.
+ */
+function initBricks() {
+    stageOne();
+}
+
+/**
+ * Destroys the brick once hit with the ball, and updates the score.
+ * A tween smoothly animates properties of an object in the gameworld, such as width
+ * or opacity.  We are adding a tween to make bricks smoothly disappear when hit by
+ * the ball.
  *
- * @returns {string}
+ * @param ball - ball object
+ * @param brick - brick object
  */
-function getRandomColor() {
-    var letters = '789ABCD';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 6)];
-    }
-    return color;
-}
+function ballHitBrick(ball, brick) {
+    var killTween = game.add.tween(brick.scale);
+    killTween.to({x:0,y:0}, 200, Phaser.Easing.Linear.None);
+    killTween.onComplete.addOnce(function(){
+        brick.kill();
+    }, this);
+    killTween.start();
 
-/**
- * Draws the ball onto the screen.
- */
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(ballPositionX, ballPositionY, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = ballColor;
-    ctx.fill();
-    ctx.closePath();
-}
+    score += scoreIncrement;
+    scoreText.setText('Points: ' + score);
 
-/**
- * Draws the paddle onto the screen.
- */
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = paddleColor;
-    ctx.fill();
-    ctx.closePath();
-}
-
-/**
- * Draws the bricks (enemies) onto the canvas.
- * Each brick is given an ballPositionX, ballPositionY, and status component.
- * Status means if it is a 1, then draw the brick, else, don't.
- */
-function createBricks2dArray() {
-    for (column = 0; column < brickColumnCount; column++) {
-        bricks[column] = [];
-        for (row = 0; row < brickRowCount; row++) {
-            bricks[column][row] = {
-                x: 0,
-                y: 0,
-                status: 1
-            };
-        }
+    if(score === brickInfo.count.row * brickInfo.count.col * scoreIncrement) {
+        alert('You won the game, congratulations!');
+        location.reload();
     }
 }
 
 /**
- * Draws the bricks onto the screen if the status of the brick is a 1.
- */
-function drawBricks() {
-    for (column = 0; column < brickColumnCount; column++) {
-        for (row = 0; row < brickRowCount; row++) {
-            if (bricks[column][row].status == 1) {
-                var brickX = (column * (brickWidth + brickPadding)) + brickOffsetLeft;
-                var brickY = (row * (brickHeight + brickPadding)) + brickOffsetTop;
-                bricks[column][row].x = brickX;
-                bricks[column][row].y = brickY;
-
-                ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = brickColor;
-                ctx.fill();
-                ctx.closePath();
-            }
-        }
-    }
-}
-
-// event listeners
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
-
-/**
- * Handles the action when a key is pressed down.
+ * Plays the animation 'wobble' when ball bounces off the paddle.
+ * The larger the distance betwen the center of the paddle and the place where
+ * the ball hits, the stronger the bounce either to the left or right.
  *
- * @param e
+ * @param ball
+ * @param paddle
  */
-function keyDownHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = true;
-    } else if (e.keyCode == 37) {
-        leftPressed = true;
+function ballHitPaddle(ball, paddle) {
+    ball.animations.play('wobble');
+    ball.body.velocity.x = -1 * 5 * (paddle.x - ball.x);
+}
+
+/**
+ * When the ball leaves the screen, which is the bottom wall, reduce number of lives.
+ * If there are still lives left, set text, and make visible lifeLostText, which allows
+ * user to decide to continue or not.  Reset the ball and paddle, and only invoke the
+ * addOnce function when user clicks a mouse or a key.
+ */
+function ballLeaveScreen() {
+    lives--;
+    if (lives) {
+        livesText.setText('Lives: ' + lives);
+        lifeLostText.visible = true;
+        ball.reset(game.world.width * 0.5, game.world.height - 25);
+        paddle.reset(game.world.width * 0.5, game.world.height - 5);
+        game.input.onDown.addOnce(function() {
+            lifeLostText.visible = false;
+            ball.body.velocity.set(velocityX, velocityY);
+        }, this);
+    } else {
+        alert('You Lost, Game Over.');
+        location.reload();
     }
 }
 
 /**
- * Handles the action when a key is released.
- *
- * @param e
+ * When the button is pressed, remove the button, set balls initial velocity,
+ * and set playing variable to true.
  */
-function keyUpHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = false;
-    } else if (e.keyCode == 37) {
-        leftPressed = false;
-    }
-}
-
-/**
- * Handles the mouse movement.
- *
- * @param e
- */
-function mouseMoveHandler(e) {
-    var relativeX = e.clientX - canvas.offsetLeft;
-    if (relativeX > 0 && relativeX < canvas.width) {
-        paddleX = relativeX - paddleWidth / 2;
-    }
-}
-
-/**
- * Collision detection for when the center of the ball hits a brick.
- *
- * These conditions must be met:
- * The ballPositionX position of the ball is greater than the ballPositionX position of the brick.
- * The ballPositionX position of the ball is less than the ballPositionX position of the brick plus its width.
- * The ballPositionY position of the ball is greater than the ballPositionY position of the brick.
- * The ballPositionY position of the ball is less than the ballPositionY position of the brick plus its height.
- *
- * If the score is equal to the number of bricks in the game, it will alert a
- * congratulations message, and start the game over.
- */
-function collisionDetection() {
-    for (column = 0; column < brickColumnCount; column++) {
-        for (row = 0; row < brickRowCount; row++) {
-            var b = bricks[column][row];
-            if (b.status == 1) {
-                if (ballPositionX > b.x - ballRadius && ballPositionX < b.x + brickWidth + ballRadius && ballPositionY > b.y && ballPositionY < b.y + brickHeight + ballRadius) {
-                    dy = -dy;
-                    b.status = 0;
-                    score++;
-                    playAudio('block_destroy.wav');
-                    if (score == brickRowCount * brickColumnCount) {
-                        playAudio('winner.wave');
-                        alert("YOU WIN, CONGRATS!");
-                        document.location.reload();
-                    }
-                }
-            }
-        }
-    }
-}
-
-/**
- * Draws the scoreboard for the game.
- *
- * Adds a point for every brick destroyed in the function collisionDetection().
- */
-function drawScore() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#004465";
-    ctx.fillText("Score " + score, 8, 20);
-}
-
-function drawLives() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#dd40ce";
-    ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
-}
-
-/**
- * clearRect(x1, y1, x2, y2) clears specified area of a rectangle
- */
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBricks();
-    drawBall();
-    drawPaddle();
-    drawScore();
-    drawLives();
-    collisionDetection();
-
-    ballPositionX += dx;
-    ballPositionY += dy;
-
-    // first condition = top wall, second condition = bottom wall
-    // sets ball to random color when hitting a wall
-    // when second condition is hit, meaning the ball hits the bottom wall,
-    // it checks if it hit the paddle, and if its not within bounds of paddle width,
-    // alerts game over if all the lives are gone
-    if (ballPositionY + dy < ballRadius) {
-        dy = -dy;
-        ballColor = getRandomColor();
-    } else if (ballPositionY + dy > canvas.height - ballRadius) {
-        if (ballPositionX > paddleX && ballPositionX < paddleX + paddleWidth) {
-            dy = -dy;
-        } else {
-            lives--;
-            if (!lives) {
-                stopMusic();
-                playAudio('lost.wav');
-                //alert("GAME OVER");
-                //document.location.reload();
-            } else {
-                ballPositionX = canvas.width / 2;
-                ballPositionY = canvas.height- 30;
-                dx = 2;
-                dy = -2;
-                paddleX = (canvas.width - paddleWidth) / 2;
-            }
-        }
-    }
-
-    // first condition = left wall, second condition = right wall
-    // sets ball to random color when hitting a wall
-    if (ballPositionX + dx < ballRadius || ballPositionX + dx > canvas.width - ballRadius) {
-        dx = -dx;
-        ballColor = getRandomColor();
-    }
-
-    // if user tries to go out of bounds on right wall, this will prevent it
-    // also if user tries to go out of bounds on left wall, will also prevent it
-    if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += paddleSpeed;
-    } else if (leftPressed && paddleX > 0) {
-        paddleX -= paddleSpeed;
-    }
-
-    requestAnimationFrame(draw);
-}
-
-// runs function over and over again using requestAnimationFrame(), which lets browser
-// render the framerates and shapes when needed
 function startGame() {
-    playMusic();
-    draw();
+    startButton.destroy();
+    ball.body.velocity.set(velocityX, velocityY);
+    playing = true;
+    music.play();
+}
+
+/**
+ * Example stage.  Uses JSON data to retrieve stage information.
+ */
+function stageOne() {
+    brickInfo = {
+        width: levelData.brickSettings.brickWidth,
+        height: levelData.brickSettings.brickHeight,
+        count: {
+            row: levelData.brickSettings.brickRows,
+            col: levelData.brickSettings.brickColumns
+        },
+        offset: {
+            top: levelData.brickSettings.brickOffsetTop,
+            left: levelData.brickSettings.brickOffsetLeft
+        },
+        padding: levelData.brickSettings.brickPadding
+    }
+
+    bricks = game.add.group();
+
+    for (c = 0; c < brickInfo.count.col; c++) {
+        for (r = 0; r < brickInfo.count.row; r++) {
+            if (c % 2 == 0) {
+                var brickX = (r * (brickInfo.width + brickInfo.padding)) + brickInfo.offset.left;
+                var brickY = (c * (brickInfo.height + brickInfo.padding)) + brickInfo.offset.top;
+                newBrick = game.add.sprite(brickX, brickY, 'brick');
+                newBrick.scale.x = levelData.brickSettings.brickScaleX;
+                newBrick.scale.y = levelData.brickSettings.brickScaleY;
+                game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+                newBrick.body.immovable = true;
+                newBrick.anchor.set(0.5);
+                bricks.add(newBrick);
+            } else {
+
+            }
+        }
+    }
 }
